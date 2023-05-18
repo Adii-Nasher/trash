63f9a2db2f1bc5550c940f1527914df145d3281e
Aditya Kumar Singh <aditya.singh@knoldus.com>
2023-05-18 10:29:53 +0530

    Update httpRequests.scala

--- src/test/scala/httpRequests/httpRequests.scala
+++ src/test/scala/httpRequests/httpRequests.scala
@@ -4,23 +4,6 @@ import io.gatling.http.request.builder.HttpRequestBuilder
 package object httpRequests {
   val baseUrl = "https://reqres.in/api/users"
 
-  val createUserInfo: HttpRequestBuilder = http("Create user info")
-    .post(baseUrl)
-    .body(
-      StringBody(
-        """{ "name": "${name}", "job": "${job}", "id": "${id}" }"""
-      )
-    )
-    .asJson
-    .header("content-type", "application/json")
-    .check(
-      status.is (201),
-      status.not(404),
-      status.not(500),
-      status.not(400),
-      jsonPath("$.id").exists,
-  )
-
   val getUserInfo: HttpRequestBuilder = http("Get all users")
     .get(baseUrl)
     .queryParam("page", Seq("1", "2"))
@@ -34,22 +17,5 @@ package object httpRequests {
       jsonPath("$.data[0].first_name").ofType[String].exists,
       jsonPath("$.data[0].last_name").ofType[String].exists
 
-    )
-
-  val updateUserInfo: HttpRequestBuilder = http("Update user info")
-    .put(baseUrl + "/2")
-    .body(
-      StringBody(
-        """{ "name": "${name}", "job": "${job}", "id": "${id}" }"""
-      )
-    )
-    .asJson
-    .header("content-type", "application/json")
-    .check(
-      status.is(200),
-      status.not(404),
-      status.not(500),
-      status.not(400),
-      jsonPath("$.updatedAt").ofType[String].exists
     )
 }
