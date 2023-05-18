d9e89340352ed82eb1067d6a2ab5feb1f9bab605
Aditya Kumar Singh <aditya.singh@knoldus.com>
2023-05-18 10:31:29 +0530

    Update Scenario.scala

--- src/test/scala/Scenario/Scenario.scala
+++ src/test/scala/Scenario/Scenario.scala
@@ -6,17 +6,9 @@ package object Scenario {
 
   val feeder = csv("src/user_data.csv").random
 
-  val createUsersScenario: ScenarioBuilder = scenario("Create Users Scenario")
-    .feed(feeder)
-    .exec(createUserInfo)
-
   val getUsersScenario: ScenarioBuilder = scenario("Get Users Scenario")
     .exec(getUserInfo)
 
-  val updateUsersScenario: ScenarioBuilder = scenario("Update Users Scenario")
-    .feed(feeder)
-    .exec(updateUserInfo)
-
   val chainedScenario: ScenarioBuilder = scenario("Chained scenario")
-    .exec(createUsersScenario, getUsersScenario, updateUsersScenario)
+    .exec(getUsersScenario)
 }
